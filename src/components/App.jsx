f0dae7b6bafae0c0b77b947f07e1d579d743c8b0
Dmytro-1506 <kyunlik@gmail.com>
2023-04-02 20:28:37 +0200

    refreshing is back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import { AppBar } from './AppBar/AppBar';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks/useAuth';
 import Home from 'pages/Home';
 import Register from 'pages/Register';
 import Login from 'pages/Login';
 import Contacts from 'pages/Contacts';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
-  return (
+    useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <Routes>
       <Route path="/" element={<AppBar />}>
         <Route index element={<Home />} />
